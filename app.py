f8c1cb7a05cc28ca64c9d2e35c61708bfbc1b314
davelush <dave.lush@gmail.com>
2019-01-06 16:19:14 +0000

    Refactored to introduce postgres into the repository

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from autotrader.scraper import Scraper
-from autotrader.repository import Repository
+from autotrader.vehiclerepository import VehicleRepository
+import psycopg2
 
-#set up search parameters
-max_distance = 1500
-postcode = "rg315nr"
-berths = 6
-max_price = 25000
-keywords = "bunk"
 
-scraper = Scraper()
-repo = Repository()
+def main():
+    # set up search parameters
+    max_distance = 1500
+    postcode = "rg315nr"
+    berths = 6
+    max_price = 25000
+    keywords = "bunk"
 
-vans = scraper.get_vehicles(max_distance, postcode, berths, max_price, keywords)
-repo.store(vans)
+    postgres_host = "localhost"
+    postgres_port = 5432
+    postgres_db = "postgres"
+    try:
+        postgres_connection = psycopg2.connect(
+            host=postgres_host,
+            port=postgres_port,
+            dbname=postgres_db
+        )
+        scraper = Scraper()
+        repo = VehicleRepository(postgres_connection)
+
+        vans = scraper.get_vehicles(max_distance, postcode, berths, max_price, keywords)
+        repo.store(vans)
+    finally:
+        postgres_connection.close()
+
+main()
